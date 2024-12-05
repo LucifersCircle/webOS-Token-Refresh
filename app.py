eef1d6f53d30768bde7ede6b7839e0be876108d3
LucifersCircle <luciferscircle@gmail.com>
2024-12-05 15:01:11 -0800

    Update app.py
    debug flag

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def add_key():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
